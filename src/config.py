45af7f60ff5fa064204db36bfeb84405e97f2793
aabelnegash <negashabel63@gmail.com>
2026-02-22 14:14:28 -0500

    feat: lock clean output schema

---
+++ src/config.py
@@ -0,0 +1,24 @@
+# src/config.py
+
+KEEP_COLUMNS = [
+    "provider_id",
+    "hospital_name",
+    "address",
+    "city",
+    "state",
+    "zip_code",
+    "county_name",
+    "phone_number",
+    "hospital_type",
+    "hospital_ownership",
+    "emergency_services",
+    "meets_criteria_for_meaningful_use_of_ehrs",
+    "hospital_overall_rating",
+    "mortality_national_comparison",
+    "safety_of_care_national_comparison",
+    "readmission_national_comparison",
+    "patient_experience_national_comparison",
+    "effectiveness_of_care_national_comparison",
+    "timeliness_of_care_national_comparison",
+    "efficient_use_of_medical_imaging_national_comparison",
+]
